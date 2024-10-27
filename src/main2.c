7bbf22767ee58393661fd6bc11bf6152daab9af0
rayan ferradi <ferradi.r@gmail.com>
2024-10-27 02:01:53 +0100

    fix env verification

--- src/main2.c
+++ src/main2.c
@@ -6,13 +6,13 @@
 /*   By: rferradi <rferradi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 01:20:28 by rferradi          #+#    #+#             */
-/*   Updated: 2022/12/19 02:08:52 by rferradi         ###   ########.fr       */
+/*   Updated: 2024/10/27 02:01:44 by rferradi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-int	error_msg(char *msg, char *name)
+#include <stdio.h>
+int error_msg(char *msg, char *name)
 {
 	if (!name)
 	{
@@ -23,15 +23,15 @@ int	error_msg(char *msg, char *name)
 	exit(1);
 }
 
-int	main(int argc, char const *argv[], char **envp)
+int main(int argc, char const *argv[], char **envp)
 {
 	t_data	data;
-	int		i;
-	int		status;
+	int		 	i;
+	int			status;
 
 	i = -1;
 	status = 0;
-	if ((argc != 5) || (!envp) || (!deal_parse(&data, argv, envp, argc)))
+	if ((argc != 5) || (!*envp) || (!deal_parse(&data, argv, envp, argc)))
 		return (0);
 	while (data.cmds[++i])
 		waitpid(data.cmds[i]->pid, &status, 0);
